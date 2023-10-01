aee35099180f3183d52c8c722f6b956ae6697e1e
michellelestuny <112867186+michellelestuny@users.noreply.github.com>
2023-10-01 14:12:32 +0700

    71190415_tugas 2

---
+++ 71190415_tugas 2/env.js
@@ -0,0 +1,13 @@
+// 1. Membuat Env Variable sesuai data yang diregister
+const cat_env = {
+  API_KEY: "your_cat_api_key",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+const dog_env = {
+  API_KEY: "your_dog_api_key",
+  endpoint: "https://api.thedogapi.com/",
+};
+
+// 2. Export dog_env
+export { dog_env };
